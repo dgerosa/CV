bb5987bfc35edd843975a7eb26bc0cbcb316e114
Davide Gerosa <dgerosa@caltech.edu>
2017-04-12 11:20:25 -0700

    my bib file

--- clean_bib.sh
+++ clean_bib.sh
@@ -7,3 +7,4 @@ sed -ie 's/\\mnrasl/Monthly Notices of the Royal Astronomical Society Letters/g'
 sed -ie 's/\\prd/Physical Review D/g' publist.bib
 sed -ie 's/\\prl/Physical Review Letters/g' publist.bib
 sed -ie 's/\\cqg/Classical and Quantum Gravity/g' publist.bib
+rm publist.bibe
