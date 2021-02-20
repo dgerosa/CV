bc48dbc0cbb90e7bf235240071c95ada5123c87d
Davide Gerosa <davide.gerosa@gmail.com>
2021-02-20 15:18:34 +0000

    Test

--- makeCV.py
+++ makeCV.py
@@ -89,12 +89,9 @@ file.write(transcript)
 file.close()
 
 os.system('pdflatex transcript >/dev/null')
-
 ##########
+
 print("Cleaning...")
 os.system('rm -rf *aux *bbl *blg *out *log *synctex.gz')
-
-
-
 
 print("Done!")
