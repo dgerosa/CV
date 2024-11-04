21748918a70316f59caa500e68b7f5f6b24a0c14
Davide Gerosa <davide.gerosa@gmail.com>
2024-11-04 07:21:09 +0100

    catastrophe

--- database.py
+++ database.py
@@ -23,8 +23,19 @@ if submitted:
     papers['submitted']['data'] = []
 
     papers['submitted']['data'].append({
+        "title":    "Orbital eccentricity in general relativity from catastrophe theory",
+        "author":   "M. Boschini, N. Loutrel, D. Gerosa, G. Fumagalli",
+        "journal":  "",
+        "link":     "",
+        "arxiv":    "arXiv:2411.00098 [gr-qc]",
+        "ads":      "2024arXiv241100098B",
+        "inspire":  "Boschini:2024scu",
+        "more":     ""
+        })
+
+    papers['submitted']['data'].append({
         "title":    "Forecasting the population properties of merging black holes",
-        "author":   "V. De Renzis, F. Iacovelli, D. Gerosa, M. Mancarella, C. Pacilio.",
+        "author":   "V. De Renzis, F. Iacovelli, D. Gerosa, M. Mancarella, C. Pacilio",
         "journal":  "",
         "link":     "",
         "arxiv":    "arXiv:2410.17325 [astro-ph.HE]",
